510b2e34322c1d120d77be5ee18b5836314d685d
Markus <haxxxen@users.noreply.github.com>
2016-10-24 14:25:17 +0200

    Add files via upload

---
+++ ArtemisPS3-PRX/mem.h
@@ -0,0 +1,17 @@
+#ifndef __MEM_H__
+#define __MEM_H__
+
+
+#define MB(x)      ((x)*1024*1024)
+#define KB(x)      ((x)*1024)
+
+uint32_t get_heap(void);
+// void create_heap(int32_t size);
+void destroy_heap(void);
+void reset_heap(void);
+void *mem_alloc(uint32_t size);
+
+int32_t create_heap(int32_t size);
+
+
+#endif // __MEM_H__ 
